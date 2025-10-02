fa05b3c266c97c18c6f3da51e44550117bd15b5e
abbyortego <89620898+abbyortego@users.noreply.github.com>
2025-10-02 01:48:28 +0000

    Deploying to gh-pages from @ abbyortego/abbyortego.github.io@c67be67fed30e2c579327d03b0ec2f6963721847 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -515,7 +515,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/abbyortego", "_blank");
+          window.open("https://www.linkedin.com/in/abby-ortego", "_blank");
         },
       },{
         id: 'social-scholar',
